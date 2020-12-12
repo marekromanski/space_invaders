5c95679ffd8b95b325ce8099cc044973642a26e1
Marek Romański <marek.romanski@huuugegames.com>
2020-12-12 11:32:24 +0100

    Flow for loading the scene after all dependencies are loaded

--- Assets/Scripts/GameLoader.cs
+++ Assets/Scripts/GameLoader.cs
@@ -1,6 +1,8 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameLoader
 {
@@ -12,7 +14,36 @@ public class GameLoader
 
     private async UniTask LoadGame()
     {
-        await UniTask.Delay(100);
+        var loadSceneTask = LoadMainMenu();
+
+        var depenencies = LoadDependencies(); 
+
+        await UniTask.WhenAll(depenencies);
+        Debug.Log("Dependencies loaded");
+        var result = await loadSceneTask;
+        Debug.Log("Scene loaded");
+        result.allowSceneActivation = true;
         Debug.Log("Game Loaded");
+    }
+
+    private IEnumerable<UniTask> LoadDependencies()
+    {
+        Debug.Log("Loading dependencies");
+        var dependencies = new List<UniTask>();
+
+        return dependencies;
+    }
+
+    private async UniTask<AsyncOperation> LoadMainMenu()
+    {
+        var loadSceneOperation = SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+
+        loadSceneOperation.allowSceneActivation = false;
+        while (!loadSceneOperation.isDone)
+        {
+            await UniTask.Delay(500);
+        }
+
+        return loadSceneOperation;
     }
 }
