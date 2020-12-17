0f5a6ec49bde9e822a6cf0fe0ee32d8aaf54d1b4
Marek Romański <mromanski91@gmail.com>
2020-12-17 15:00:51 +0100

    Imported DOTween

---
+++ Assets/Scripts/Battles/Entities/Enemies/EnemiesRow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Battles.Entities.Enemies
+{
+    public class EnemiesRow
+    {
+        public readonly EnemyType type;
+        public readonly List<EnemyEntity> enemies;
+            
+        public EnemyEntity MostLeftEntity { get; private set; }
+
+        public EnemiesRow(EnemyType enemyType, List<EnemyEntity> entities)
+        {
+            this.type = type;
+            this.enemies = entities;
+        }
+    }
+}
